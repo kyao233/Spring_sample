bc21e18c4b92a09dd3c5e0d2c125ee24c1dda935
kyao4 <kyao4@hawk.iit.edu>
2017-12-15 20:09:02 +0800

    init

---
+++ src/main/java/com/spring/Spring/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.spring.Spring;
+
+public class HelloWorld {
+
+	private String name = null;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
