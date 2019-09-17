d34eef3b73b692dbf181fed6db37a8d5d1c65028
kyao233 <kyao233@163.com>
2019-09-17 21:46:07 +0800

    add anonymous to work around form login

--- springsecurity/src/main/java/com/xdclass/config/WebSecurityConfig.java
+++ springsecurity/src/main/java/com/xdclass/config/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.xdclass.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -11,18 +13,22 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
  * @date 2019/9/16 17:07
  */
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     /**
      * Simple authentication config.
      * @return
      * @throws Exception
      */
-//    @Bean
-//    public UserDetailsService userDetailsService() throws Exception {
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//        manager.createUser(User.withUsername("user").password("password").roles("USER").build());
-//        return manager;
-//    }
+    @Override
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("James").password("12345").roles("USER").build());
+        return manager;
+    }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.anonymous().and().formLogin().and().httpBasic();
+    }
 }
