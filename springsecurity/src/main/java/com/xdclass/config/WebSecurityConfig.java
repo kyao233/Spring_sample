ca12e9a4089ff06f91ac76918645a37293c470fe
kyao233 <kyao233@163.com>
2019-09-18 23:19:36 +0800

    add more pattern url and formlogin

--- springsecurity/src/main/java/com/xdclass/config/WebSecurityConfig.java
+++ springsecurity/src/main/java/com/xdclass/config/WebSecurityConfig.java
@@ -36,9 +36,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.authorizeRequests()//this uri is based on contextpath
+                .antMatchers("/**/index").permitAll()
                 .antMatchers("/**").fullyAuthenticated()
                 .and()
-                .httpBasic();
+                .formLogin();
     }
 }
